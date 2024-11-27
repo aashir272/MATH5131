5a908a496c68c51d9f5b7908b7364b422437c703
luchtele <lea.luchterhand@gmail.com>
2024-11-27 15:34:54 -0500

    comparison of different scenarios with one extreme case and one base case each

--- model.py
+++ model.py
@@ -3,33 +3,19 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 # Parameters
-a = [1.5, 1.5] # birth rate humans
-b_h = [0.01, 0.2] # death rate humans
-g = [0.01, 0.01]   # gamma capacity factor
-c_i = [0.01, 0.5]  # immunization rate
-c_z = [0.03, 0.02]   # zombification rate
-b_z = [0.01, 0.01]  # death rate zombies
+a =     [1.5, 1.5, 1.5, 1.5 ] # birth rate humans
+b_h =   [0.2, 0.2, 0.2, 0.2 ] # death rate humans
+g =     [0.01, 0.01, 0.01, 0.01]   # gamma capacity factor
+c_i =   [0.5,  0.7, 0.5, 0.5]  # immunization rate
+c_z =   [0.02, 0.02, 0.03, 0.02]   # zombification rate
+b_z =   [0.01, 0.01, 0.01, 0.015] # death rate zombies
 
 
 
-# System of differential equations base case
-def model1(y, t, a, b_h, g, c_i, c_z,b_z):
-    H, I, Z = y
-    dHdt = H *a - H*( b_h + c_i + g * (H + I)+ c_z * Z )
-    dIdt = c_i*H + I * (a- b_h - g * (H + I))
-    dZdt = (c_z * H - b_z * (H + I)) * Z
-    return [dHdt, dIdt, dZdt]
 
-# System of differential equations immune have decreased birth rate
-def model2(y, t, a, b_h, g, c_i, c_z,b_z):
-    H, I, Z = y
-    dHdt = H *a - H*( b_h + c_i + g * (H + I)+ c_z * Z )
-    dIdt = c_i*H + I * (a*0.5- b_h - g * (H + I))
-    dZdt = (c_z * H - b_z * (H + I)) * Z
-    return [dHdt, dIdt, dZdt]
 
 # System of differential equations for immune give birth to none immune
-def model3(y, t, a, b_h, g, c_i, c_z,b_z):
+def model(y, t, a, b_h, g, c_i, c_z,b_z):
     H, I, Z = y
     dHdt = (H+I) *a - H*( b_h + c_i + g * (H + I)+ c_z * Z )
     dIdt = c_i*H + I * (- b_h - g * (H + I))
@@ -45,33 +31,51 @@ y0 = [H0, I0, Z0]
 H=[]
 I=[]
 Z=[]
-scenarios = [0,1]
+scenarios = [0,1,2,3]
 
-t = np.linspace(0, 10, 100)
+t = np.linspace(0, 40, 100)
 
 
-for s in scenarios: # model 1
-    solution = odeint(model1, y0, t, args= (a[s],b_h[s],g[s], c_i[s], c_z[s], b_z[s]))
+
+for s in scenarios: 
+    solution = odeint(model, y0, t, args= (a[s],b_h[s],g[s], c_i[s], c_z[s], b_z[s]))
     H.append( solution[:,0])
     I.append( solution[:,1])
     Z.append( solution[:,2])
 
-for s in scenarios: #model 2
-    solution = odeint(model2, y0, t, args= (a[s],b_h[s],g[s], c_i[s], c_z[s], b_z[s]))
-    H.append( solution[:,0])
-    I.append( solution[:,1])
-    Z.append( solution[:,2])
+#equi solutions
+solution = odeint(model, [1,1,150], t, args= (a[0],b_h[0],g[0], c_i[0], c_z[0], b_z[0]))
+He = []
+Ie= []
+Ze = []
+He.append( solution[:,0])
+Ie.append( solution[:,1])
+Ze.append( solution[:,2])
 
-for s in scenarios: # model 3
-    solution = odeint(model3, y0, t, args= (a[s],b_h[s],g[s], c_i[s], c_z[s], b_z[s]))
-    H.append( solution[:,0])
-    I.append( solution[:,1])
-    Z.append( solution[:,2])
+#increased immunization
+solution2 = odeint(model, [1,1,100], t, args= (a[1],b_h[1],g[1], c_i[1], c_z[1], b_z[1]))
+He.append( solution2[:,0])
+Ie.append( solution2[:,1])
+Ze.append( solution2[:,2])
+
+#increased zombiefication 
+solution3 = odeint(model, [1,1,1], t, args= (a[2],b_h[2],g[2], c_i[2], c_z[2], b_z[2]))
+He.append( solution3[:,0])
+Ie.append( solution3[:,1])
+Ze.append( solution3[:,2])
+
+#increased kill rate 
+solution3 = odeint(model, [1,1,100], t, args= (a[3],b_h[3],g[3], c_i[3], c_z[3], b_z[3]))
+He.append( solution3[:,0])
+Ie.append( solution3[:,1])
+Ze.append( solution3[:,2])
+
+# equilibirum solution at 15,15,100
 
 #plots
-plt.figure(figsize=(25, 10))
+plt.figure(figsize=(15, 10))
 
-plt.subplot(3, 2, 1)
+plt.subplot(4, 2, 1)
 plt.plot(t, H[0], label='Humans (H)', color='blue')
 plt.plot(t, I[0], label='Immune Humans (I)', color='red')
 plt.plot(t, Z[0], label='Zombies (Z)', color='green')
@@ -81,57 +85,80 @@ plt.legend()
 plt.title('Base case 1')
 plt.grid(True)
 
+plt.subplot(4, 2, 2)
+plt.plot(t, He[0], label='Humans (H)', color='blue')
+plt.plot(t, Ie[0], label='Immune Humans (I)', color='red')
+plt.plot(t, Ze[0], label='Zombies (Z)', color='green')
+plt.xlabel('Time')
+plt.ylabel('Population')
+plt.legend()
+plt.title('extreme case')
+plt.grid(True)
 
-plt.subplot(3, 2, 2)
+# increased immunization
+
+plt.subplot(4, 2, 3)
 plt.plot(t, H[1], label='Humans (H)', color='blue')
 plt.plot(t, I[1], label='Immune Humans (I)', color='red')
 plt.plot(t, Z[1], label='Zombies (Z)', color='green')
 plt.xlabel('Time')
 plt.ylabel('Population')
 plt.legend()
-plt.title('Base case 2')
+plt.title('increased immunization')
 plt.grid(True)
-##############################################scenario 2
-plt.subplot(3, 2, 3)
+
+plt.subplot(4, 2, 4)
+plt.plot(t, He[1], label='Humans (H)', color='blue')
+plt.plot(t, Ie[1], label='Immune Humans (I)', color='red')
+plt.plot(t, Ze[1], label='Zombies (Z)', color='green')
+plt.xlabel('Time')
+plt.ylabel('Population')
+plt.legend()
+plt.title('increased immunization extreme')
+plt.grid(True)
+
+
+# increased zombiefication
+plt.subplot(4, 2, 5)
 plt.plot(t, H[2], label='Humans (H)', color='blue')
 plt.plot(t, I[2], label='Immune Humans (I)', color='red')
 plt.plot(t, Z[2], label='Zombies (Z)', color='green')
 plt.xlabel('Time')
 plt.ylabel('Population')
 plt.legend()
-plt.title('decreased birth rate 1')
+plt.title('increased zombification')
 plt.grid(True)
 
-plt.subplot(3, 2, 4)
+plt.subplot(4, 2, 6)
+plt.plot(t, He[2], label='Humans (H)', color='blue')
+plt.plot(t, Ie[2], label='Immune Humans (I)', color='red')
+plt.plot(t, Ze[2], label='Zombies (Z)', color='green')
+plt.xlabel('Time')
+plt.ylabel('Population')
+plt.legend()
+plt.title('increased zombification extreme case')
+plt.grid(True)
+
+
+# increased kill rate
+plt.subplot(4, 2, 7)
 plt.plot(t, H[3], label='Humans (H)', color='blue')
 plt.plot(t, I[3], label='Immune Humans (I)', color='red')
 plt.plot(t, Z[3], label='Zombies (Z)', color='green')
 plt.xlabel('Time')
 plt.ylabel('Population')
 plt.legend()
-plt.title('decreased birth rate 2')
+plt.title('increased zombie kill rate')
 plt.grid(True)
 
-
-##############################################scenario 2
-plt.subplot(3, 2, 5)
-plt.plot(t, H[4], label='Humans (H)', color='blue')
-plt.plot(t, I[4], label='Immune Humans (I)', color='red')
-plt.plot(t, Z[4], label='Zombies (Z)', color='green')
+plt.subplot(4, 2, 8)
+plt.plot(t, He[3], label='Humans (H)', color='blue')
+plt.plot(t, Ie[3], label='Immune Humans (I)', color='red')
+plt.plot(t, Ze[3], label='Zombies (Z)', color='green')
 plt.xlabel('Time')
 plt.ylabel('Population')
 plt.legend()
-plt.title('no immune births 1')
-plt.grid(True)
-
-plt.subplot(3, 2, 6)
-plt.plot(t, H[5], label='Humans (H)', color='blue')
-plt.plot(t, I[5], label='Immune Humans (I)', color='red')
-plt.plot(t, Z[5], label='Zombies (Z)', color='green')
-plt.xlabel('Time')
-plt.ylabel('Population')
-plt.legend()
-plt.title('no immune births 2')
+plt.title('increased zombie kill rate')
 plt.grid(True)
 
 
